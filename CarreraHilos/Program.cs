c7e2ee28553b10535fa45bfbc5da1a4afda6df87
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-04-02 12:22:08 -0600

    Agregar proyecto

---
+++ CarreraHilos/Program.cs
@@ -0,0 +1,7 @@
+﻿using System; //importación de librerías, permnite usar funciones básicas (writeLine)
+using System.Threading;//importa la funcionalidad necesaria para utilizar hilos 
+class Program
+{
+   
+    }
+}
